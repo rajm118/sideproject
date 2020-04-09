ede7917ffcedc37518b1ad82b91351bac286cc6b
rodio346 <vvyy346@gmail.com>
2020-04-09 20:37:08 +0530

    Race track created

--- games/Basic/basic.py
+++ games/Basic/basic.py
@@ -1,15 +1,29 @@
 import pygame
 
 pygame.init()
+carryOn = True
+clock = pygame.time.Clock()
+# Define some colors
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+size = (700, 700)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My First Game")
 
-screen = pygame.display.set_mode([500, 500])
-running = True
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            carryOn = False  # Flag that we are done so we exit this loop
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-   screen.fill((255, 255, 255))
-   pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-   pygame.display.flip()
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+
+    pygame.display.flip()
+
+    clock.tick(50)
 pygame.quit()

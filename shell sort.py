b5cb4fa46e6a11e4827cd12498f69c1374fc1496
rodio346 <vvyy346@gmail.com>
2020-04-14 23:06:18 +0530

    Shell sort

---
+++ shell sort.py
@@ -0,0 +1,30 @@
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+
+      sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+
+        currentvalue = alist[i]
+        position = i
+
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+
+        alist[position]=currentvalue
+
+alist=[]
+T=int(input("Enter the length of list "))
+for i in range(T):
+    alist[i]=int(input())
+shellSort(alist)
+print(alist)

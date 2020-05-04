0929eced0207d5065f205c63cb975feca27bb330
rodio346 <vvyy346@gmail.com>
2020-05-05 01:01:09 +0530

    adding keystrokes

--- games/SpaceInvadors/main.py
+++ games/SpaceInvadors/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 # create screen use tuple to initialize the size of the screen
@@ -9,17 +10,35 @@ pygame.display.set_caption("Space Invaders")
 # create the variable to load the image and then use the image
 icon = pygame.image.load('technology.png')
 pygame.display.set_icon(icon)
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 750)
+enemyY = random.randint(50, 200)
 
-#initialzing the player
-playerImg= pygame.image.load('player.png')
-#initialize the position 
+# initialzing the player
+playerImg = pygame.image.load('player.png')
+# initialize the position
 playerX = 370
 playerY = 400
-playerX_change= 0.1
-playerY_change= 0.1
-def player(x,y):
-    screen.blit(playerImg,(x,y))
+playerX_change = 0.0
+playerY_change = 0.1
 
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+def enemy_movement(enemyX, enemyY):
+    enemy_change = 0.3
+    enemyX += enemy_change
+    if enemyX >= 750:
+        enemyY += 32
+    elif enemyX < 32:
+        enemyY += 32
+    print(enemyX)
 
 running = True
 while running:
@@ -29,19 +48,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # adding keystroke
+        # adding keystroke
         if event.type == pygame.KEYDOWN:
-            if event.key ==pygame.K_LEFT:
-                print("left arraow pressed")
+            if event.key == pygame.K_LEFT:
+                print("Left arrow pressed")
                 playerX_change = -0.1
 
-            if event.key ==pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 playerX_change = 0.1
-                print("left arraow pressed")
+                print("Right arrow pressed")
         if event.type == pygame.KEYUP:
-            if event.key ==pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 print("Keystroke has been released ")
+                playerX_change = 0
     playerX += playerX_change
-    player(playerX,playerY)
-    #necessary in order to update the screen else it will show previous data only
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 768:
+        playerX = 768
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    enemy_movement(enemyX, enemyY)
+    # necessary in order to update the screen else it will show previous data only
     pygame.display.update()

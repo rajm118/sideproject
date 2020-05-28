ce39b7ecb55fa9e62e68d33bb00c1c5f0199da55
rodio346 <vvyy346@gmail.com>
2020-05-28 23:58:56 +0530

    bullets

--- games/SpaceInvadors/main.py
+++ games/SpaceInvadors/main.py
@@ -4,24 +4,35 @@ import random
 pygame.init()
 # create screen use tuple to initialize the size of the screen
 screen = pygame.display.set_mode((800, 600))
-background =pygame.image.load('background.png')
+background = pygame.image.load('background.png')
 # title and icon
 pygame.display.set_caption("Space Invaders")
 # create the variable to load the image and then use the image
 icon = pygame.image.load('technology.png')
 pygame.display.set_icon(icon)
-#Enemy
+# Enemy
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0, 750)
 enemyY = random.randint(50, 200)
-enemyX_change = 0.3
+enemyX_change = 4
 enemyY_change = 40
+
+# Bullet
+
+# Ready state means you can fire now
+# Fire state bullet is already moving
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 40
+bullet_state = 'ready'
 
 # initialzing the player
 playerImg = pygame.image.load('player.png')
 # initialize the position
 playerX = 370
-playerY = 400
+playerY = 480
 playerX_change = 0.0
 playerY_change = 0.1
 
@@ -34,15 +45,20 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = 'fire'
+    screen.blit(bulletImg, (x + 16, y + 16))
 
-def player_movemnet(player_X,player_Y):
+
+def player_movemnet(player_X, player_Y):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
             print("Left arrow pressed")
-            playerX_change = -0.1
+            playerX_change = -5
 
         if event.key == pygame.K_RIGHT:
-            playerX_change = 0.1
+            playerX_change = 5
             print("Right arrow pressed")
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -53,7 +69,7 @@ def player_movemnet(player_X,player_Y):
 running = True
 while running:
     screen.fill((0, 0, 255))
-    screen.blit(background,(0,0))
+    screen.blit(background, (0, 0))
     enemy(enemyX, enemyY)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,17 +78,18 @@ while running:
         # adding keystroke
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print("Left arrow pressed")
-                playerX_change = -0.1
+                playerX_change = -5
 
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
-                print("Right arrow pressed")
+                playerX_change = 5
+
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                print("Keystroke has been released ")
                 playerX_change = 0
-#Establishing boundaries for both enemy and player
+    # Establishing boundaries for both enemy and player
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
@@ -81,12 +98,16 @@ while running:
 
     enemyX += enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.5
+        enemyX_change = 3
         enemyY += enemyY_change
-    if enemyX >= 768:
-        enemyX_change = -0.5
-        enemyY+= enemyY_change
+    elif enemyX >= 768:
+        enemyX_change = -3
+        enemyY += enemyY_change
 
+    #Movement of bullet
+    if bullet_state== "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     # necessary in order to update the screen else it will show previous data only
